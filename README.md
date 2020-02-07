ca3ddff721449ce8a21e8c3d6eec9e79fcd146bf
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-02-07 20:48:12 +0100

    wip

--- README.md
+++ README.md
@@ -1,22 +1,43 @@
+# how to get twitter user id
+
 https://tweeterid.com/ -> @AdamBezecny  -> 1220063080152997889
+
 http://gettwitterid.com/ -> AdamBezecny -> 1220063080152997889
 
-https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/getting-started-with-webhooks
-https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/securing-webhooks
-https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/managing-webhooks-and-subscriptions
-etc.
+# configuring webhook
 
+https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/getting-started-with-webhooks
+
+https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/securing-webhooks
+
+https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/managing-webhooks-and-subscriptions
+
+etc.
 
 https://developer.twitter.com/en/docs/tutorials/customer-engagement-application-playbook
 
+# libraries
+
 https://www.npmjs.com/package/twitter-webhooks
+
 https://www.npmjs.com/package/twitter
+
 https://www.npmjs.com/package/twit
 
+# direct messages
 https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/new-event
+
 https://developer.twitter.com/en/docs/direct-messages/quick-replies/api-reference/options
+
 https://developer.twitter.com/en/docs/direct-messages/buttons/api-reference/buttons
+
 https://developer.twitter.com/en/docs/direct-messages/typing-indicator-and-read-receipts/overview
 
+
 # see this for typing indicator and message read payloads!
+
 https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/twitter-account-activity-tutorial-autohook
+
+# twitter oauth
+
+https://developer.twitter.com/en/docs/basics/authentication/oauth-1-0a/obtaining-user-access-tokens
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require ('body-parser')
+const util = require('util')
 const request = require('request')
+// we must use promisify, without it it does not work properly
+const post = util.promisify(request.post)
 const port = 3000
 const crypto = require('crypto')
 const twitterWebhooks = require('twitter-webhooks')
@@ -13,30 +16,18 @@ const twitterClient = new Twit({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-let _twitter_time_minus_local_time_ms = 0
-
 const delay = ms => new Promise(res => setTimeout(res, ms))
 
 const TWITTER_WEBHOOK_ROUTE = '/webhook/twitter'
 
-const updateClockOffsetFromResponse =  (resp) => {
-    if (resp && resp.headers && resp.headers.date &&
-        new Date(resp.headers.date).toString() !== 'Invalid Date'
-    ) {
-      const twitterTimeMs = new Date(resp.headers.date).getTime()
-      _twitter_time_minus_local_time_ms = twitterTimeMs - Date.now()
-    }
-}
-
+// https://glitch.com/edit/#!/twitter-autohook-tutorial
 const getOauthData = () => {
-    const oauth_ts = Date.now() + _twitter_time_minus_local_time_ms;
-
     return {
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         token: process.env.TWITTER_ACCESS_TOKEN,
         token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-        timestamp: Math.floor(oauth_ts/1000).toString(),
+        // timestamp: Math.floor((Date.now()) / 1000).toString()
     }    
 }
 
@@ -46,40 +37,58 @@ const get_challenge_response = (crc_token, consumer_secret) => {
 }
 
 const directMessageHandler = async (msg) => {
-    console.log('direct_message ' + JSON.stringify(msg, null, 2))
+    // console.log('direct_message ' + JSON.stringify(msg, null, 2))
     const senderId = msg.message_create.sender_id
     const recipientId = msg.message_create.target.recipient_id
     const message = msg.message_create.message_data.text
     console.log('senderId ' + senderId)
-    console.log('recipient_id ' + recipientId)
+    console.log('recipientId ' + recipientId)
     console.log('message ' + message)
 
-    console.log('responding with echo...')
-    await sendTypingIndicator(senderId)
-    await delay(4000)
-    const response = await sendDirectMessageText(senderId, recipientId, 'echo: ' + message)
-    updateClockOffsetFromResponse(response)
-    console.log('responded with echo...')
+    try {
+        console.log('marking as read')
+        await markAsRead(msg.id, recipientId) //by common sense senderId should be here but it does not work then
+        console.log('sending typing indicator...')
+        let response = await indicateTyping(recipientId)////by common sense senderId should be here but it does not work then
+        console.log('typing indicator sent!')
+        // await delay(5000)
+        console.log('responding with echo...')
+        // response = await sendDirectMessageText(senderId, recipientId, 'echo: ' + message)
+        response = await sendDirectMessageText2(senderId, recipientId, 'echo: ' + message)
+        console.log('responded with echo')
+        console.log('-------------------------------------')
+    } catch (err) {
+        console.log('directMessageHandler error' + err.message)
+        console.log(err)
+    }
+
 }
 
-const sendTypingIndicator = async (recipientId) => {
+const markAsRead = async (messageId, recipientId) => {
+    const requestConfig = {
+      url: 'https://api.twitter.com/1.1/direct_messages/mark_read.json',
+      form: {
+        last_read_event_id: messageId,
+        recipient_id: recipientId
+      },
+      oauth: getOauthData(),
+    }
+  
+    const response = await post(requestConfig)
+    return response
+  }
 
-    try {
-
-        const requestConfig = {
-            url: 'https://api.twitter.com/1.1/direct_messages/indicate_typing.json',
-            form: {
-              recipient_id: recipientId,
-            },
-            oauth: getOauthData(),
-          }    
-
-        const response = await request.post(requestConfig)  
-        console.log('sendTypingIndicator done ' + JSON.stringify(response))
-    } catch (err) {
-        console.log('sendTypingIndicator error ' + err.message)
-        console.log(err)
-    }    
+const indicateTyping = async (recipientId,) => {
+    const requestConfig = {
+      url: 'https://api.twitter.com/1.1/direct_messages/indicate_typing.json',
+      form: {
+        recipient_id: recipientId
+      },
+      oauth: getOauthData()
+    }
+  
+    const response = await post(requestConfig)
+    return response
 }
 
 // see https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/new-event
@@ -118,9 +127,57 @@ const sendDirectMessageText = async (recipientId, senderId, text) => {
 
     try {
         const response = await twitterClient.post('direct_messages/events/new', payload)
-        // console.log('sendDirectMessageText done ' + JSON.stringify(response))
+        return response
     } catch (err) {
         console.log('sendDirectMessageText error ' + err.message)
+        console.log(err)
+    }
+}
+
+const sendDirectMessageText2 = async (recipientId, senderId, text) => {
+    const requestConfig = {
+        url: 'https://api.twitter.com/1.1/direct_messages/events/new.json',
+        oauth: getOauthData(),
+        json: {
+          event: {
+            type: 'message_create',
+            sender_id: senderId,
+            message_create: {
+              target: {
+                recipient_id: recipientId,
+              },
+              message_data: {
+                text: text,
+              }
+            }
+          }
+        }
+      }
+    // is user types qr send back also sample quick reply response
+    if (text ==='echo: qr') {
+        console.log('adding quick reply')
+        requestConfig.json.event.message_create.message_data.quick_reply = {
+        type: 'options',
+        options: [
+          {
+            label: 'Red Bird',
+            description: 'A description about the red bird.',
+            metadata: 'external_id_1'
+          },
+          {
+            label: 'Blue Bird',
+            description: 'A description about the blue bird.',
+            metadata: 'external_id_2'
+          }
+        ]
+        }
+    }
+
+    try {
+        const response = await post(requestConfig)
+        return response
+    } catch (err) {
+        console.log('sendDirectMessageText2 error ' + err.message)
         console.log(err)
     }
 }
